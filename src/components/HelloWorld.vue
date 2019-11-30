8a8718523ed1624a9957e2d3969d31404e454cd6
UltramanAce <2496638664@qq.com>
2019-11-30 12:00:42 +0800

    建立远程连接

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <Button type="error">Error</Button>
+          ssh-keygen -t rsa -C "2496638664@qq.com"
+      git config --global user.name 'UltramanAce'
+git config --global user.email "2496638664@qq.com"
+      git remote add origin git@github.com:UltramanAce/vueNodeFullStack.git
+      git remote add origin
+      https://github.com/UltramanAce/vueNodeFullStack.git
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
